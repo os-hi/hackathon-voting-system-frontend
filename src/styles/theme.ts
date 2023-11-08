58ff73532c0ee2b356f4195186e7cba6f39622d9
Jake <110341554+os-hi@users.noreply.github.com>
2023-11-09 00:07:45 +0800

    feat: added chakraui theme colors

---
+++ src/styles/theme.ts
@@ -0,0 +1,12 @@
+// theme.js
+export default {
+    colors: {
+      transparent: "transparent",
+      black: "#000",
+      white: "#fff",
+      gray: {
+        50: "#f7fafc",
+        900: "#171923",
+      },
+    },
+  };
