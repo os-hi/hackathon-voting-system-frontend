33c976ac5c9c57c78d5fb906c1e60f5beb1d4db8
Jake <110341554+os-hi@users.noreply.github.com>
2023-11-09 01:18:05 +0800

    feat: added theme colors and screens (#15)
    * Squashed commit of the following:
    
    commit 46d171fcf50247b9faf78e0d0f7dbc718be47dbb
    Author: RedDotz20 <carlos_tabangay@outlook.com>
    Date:   Tue Nov 7 17:43:22 2023 +0800
    
        feat: base routes
    
    * feat: added theme screens and colors
    
    * feat: added logo and banner
    
    * feat: added chakraui theme colors
    
    * feat: added default css styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './styles/index.css';
-
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import { BrowserRouter } from 'react-router-dom';
 import { ErrorBoundery } from './ErrorBoundery.tsx';
+
+const theme = extendTheme({
+  colors: {
+    dark: '#1F1F21',
+    light: {
+      50: '#F6F8FB',
+      500: '#CFCFCF'
+    },
+    purple: {
+      50: '#8461EE',
+      900: '#7A52B3'
+    },
+    green: '#00B7BF',
+    blueviolet: '#5829E5',
+    gray: '#8A8A8E',
+    blue: '#4299E1',
+    orange: '#F56565'
+  },
+})
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ErrorBoundery fallback={<h1>An Error Has occured</h1>}>
       <BrowserRouter>
-        <ChakraProvider>
+        <ChakraProvider theme={theme}>
           <App />
         </ChakraProvider>
       </BrowserRouter>
