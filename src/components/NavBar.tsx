672f1103a87c0488e115eb26f5efd2a20f94c832
Jake <110341554+os-hi@users.noreply.github.com>
2023-11-10 15:45:18 +0800

    feat: added the link attribute for logo

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom";
 import Logo from "./Logo";
 import NavLinks from "./NavLinks";
 const NavBar = () => {
     return ( 
         <header className = "fixed top-0 px-12 py-3 flex justify-between items-center w-full h-15 box-border">
-            <Logo/>
+            <Link to='/home' className="flex justify-center items-center gap-3 ml-5">
+                <Logo/>
+            </Link>
             <NavLinks/>
         </header>
      );
