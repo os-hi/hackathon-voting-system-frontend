ff6567fc82719a3661bbed297e5d0e048a4fdf70
Jake <110341554+os-hi@users.noreply.github.com>
2023-11-10 21:31:38 +0800

    fix: removed unnecessary tag

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,9 +1,9 @@
 const Logo = () => {
     return (
-        <>
+        <div className="flex items-center gap-3">
             <img src="../src/assets/logo.png" alt="logo" className="w-12 h-12"/>
             <h1 className="font-bold">HackaVote</h1>
-        </>
+        </div>
      );
 }
  
