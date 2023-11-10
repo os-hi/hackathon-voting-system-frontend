6f3fb010e26e26f6695f2fe7b971f51aa0744f00
erikachan <ahelp164@gmail.com>
2023-11-10 10:51:23 +0000

    feat: #20 created a 404 page for validation

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,32 +1,27 @@
-import { useEffect, useState } from 'react';
-import { Spinner } from '@chakra-ui/react';
+import { Card,CardBody,Image,Button,Text,Link} from '@chakra-ui/react'
+
 import { useNavigate } from 'react-router-dom';
 
-export default function NotFound() {
-  const [mountLoading, setMountLoading] = useState(false);
+function NotFound() {
 
   const navigate = useNavigate();
   const redirectToLogin = () => navigate('/login', { replace: true });
 
-  useEffect(() => {
-    setTimeout(() => setMountLoading(true), 800);
-    setTimeout(() => setMountLoading(false), 2000);
-    setTimeout(() => redirectToLogin(), 2000);
-
-    () => {
-      return clearTimeout;
-    };
-  }, []);
-
   return (
-    <div className="h-screen w-screen items-center justify-center flex flex-col gap-2">
-      <h1>404 Page Not Found</h1>
-      {mountLoading && (
-        <div className="flex gap-2 items-center justify-center">
-          <Spinner size="sm" />
-          <h1>Redirecting to Login</h1>
-        </div>
-      )}
-    </div>
-  );
+        <Card align='center'>
+            <CardBody>
+              <Image
+                   src='src/assets/unauthorizedLogo.png'
+                   alt='Green double couch with wooden legs'
+                   height="250"
+                   width="300"
+               />
+                 <Text fontSize='lg'>Unauthorised Access</Text>
+                 <Text fontSize='lg'> We are sorry...</Text>
+                 <Text fontSize='md'> The page you are trying has restricted access. Please try again.</Text>
+                  <Button bg='green' onClick={redirectToLogin}> Go back</Button>
+             </CardBody>
+          </Card>
+    )
 }
+export default NotFound
