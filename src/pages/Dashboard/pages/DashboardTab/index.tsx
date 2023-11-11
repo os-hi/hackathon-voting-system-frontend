3caf878eab49f6af0940dcd26a7ed384bef8664e
Jake <110341554+os-hi@users.noreply.github.com>
2023-11-12 02:38:45 +0800

    feat: added UpcomingEvents component

--- src/pages/Dashboard/pages/DashboardTab/index.tsx
+++ src/pages/Dashboard/pages/DashboardTab/index.tsx
@@ -1,9 +1,11 @@
 import ShowEvents from '../../components/ShowEvents';
 import PastEvents from './components/PastEvents';
+import UpcomingEvents from './components/UpcomingEvents';
 const DashboardTab = (props) => {
     return ( 
             <>
                 <ShowEvents event={props.event} status = "Ongoing Events"/>
+                <UpcomingEvents />
                 <PastEvents />
             </>
      );
